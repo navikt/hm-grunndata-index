c3f01ac5808c85a2671dc06c5e18e5a49516e10a
Tuan Tran <dinh.anh.tuan.tran@nav.no>
2023-02-08 10:03:43 +0100

    Application setup src

---
+++ src/main/kotlin/no/nav/hm/grunndata/index/Application.kt
@@ -0,0 +1,13 @@
+package no.nav.hm.grunndata.db
+
+import io.micronaut.runtime.Micronaut
+
+object Application {
+    @JvmStatic
+    fun main(args: Array<String>) {
+        Micronaut.build()
+            .packages("no.nav.hm.grunndata.index")
+            .mainClass(Application.javaClass)
+            .start()
+    }
+}
---
+++ src/main/kotlin/no/nav/hm/grunndata/index/Constants.kt
@@ -0,0 +1,3 @@
+package no.nav.hm.grunndata.index
+
+const val appName = "hm-grunndata-index"
