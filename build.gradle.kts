0605ddc5f70afc1b558616e235b453ab89ca3105
Tuan Tran <dinh.anh.tuan.tran@nav.no>
2023-02-08 09:56:19 +0100

    setup build files

---
+++ build.gradle.kts
@@ -0,0 +1,124 @@
+import org.gradle.api.tasks.testing.logging.TestExceptionFormat
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+
+val jvmTarget = "17"
+val micronautVersion="3.8.3"
+val kafkaVersion = "3.2.1"
+val micrometerRegistryPrometheusVersion = "1.9.1"
+val junitJupiterVersion = "5.9.0"
+val jacksonVersion = "2.13.4"
+val logbackClassicVersion = "1.2.11"
+val logbackEncoderVersion = "7.2"
+val kafkaEmbeddedVersion = "3.2.1"
+val postgresqlVersion= "42.3.3"
+val tcVersion= "1.16.3"
+val mockkVersion = "1.13.2"
+val kotestVersion = "5.5.0"
+val apachePoiVersion = "5.2.3"
+val openSearchRestClientVersion = "1.3.7"
+val rapidsRiversVersion = "202301310932"
+
+group = "no.nav.hm"
+version = properties["version"] ?: "local-build"
+
+plugins {
+    kotlin("jvm") version "1.7.0"
+    kotlin("kapt") version "1.7.0"
+    kotlin("plugin.allopen") version "1.7.0"
+    id("java")
+    id("com.github.johnrengelman.shadow") version "7.1.0"
+    id("io.micronaut.application") version "3.6.6"
+}
+
+configurations.all {
+    resolutionStrategy {
+        failOnChangingVersions()
+    }
+}
+
+dependencies {
+    api("ch.qos.logback:logback-classic:$logbackClassicVersion")
+    api("net.logstash.logback:logstash-logback-encoder:$logbackEncoderVersion")
+    // coroutines
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-reactive")
+
+    // micronaut-data
+    implementation("io.micronaut.data:micronaut-data-jdbc")
+    implementation("jakarta.persistence:jakarta.persistence-api:2.2.3")
+    implementation("org.jetbrains.kotlin:kotlin-reflect")
+    runtimeOnly("io.micronaut.sql:micronaut-jdbc-hikari")
+    annotationProcessor("io.micronaut.data:micronaut-data-processor")
+    implementation("org.postgresql:postgresql:${postgresqlVersion}")
+    implementation("io.micronaut.flyway:micronaut-flyway")
+    implementation("io.micronaut:micronaut-runtime")
+    implementation("io.micronaut.kotlin:micronaut-kotlin-runtime")
+    implementation("io.micronaut:micronaut-http-server-netty")
+    implementation("io.micronaut:micronaut-http-client")
+    implementation("org.opensearch.client:opensearch-rest-high-level-client:${openSearchRestClientVersion}")
+    implementation("io.micronaut.cache:micronaut-cache-caffeine")
+
+    implementation("com.github.navikt:hm-rapids-and-rivers-v2-core:$rapidsRiversVersion")
+    implementation("com.github.navikt:hm-rapids-and-rivers-v2-micronaut:$rapidsRiversVersion")
+
+    testImplementation("io.mockk:mockk:$mockkVersion")
+    testImplementation("io.micronaut.test:micronaut-test-kotest5")
+    testImplementation("io.kotest:kotest-runner-junit5-jvm:$kotestVersion")
+    testImplementation("io.kotest:kotest-assertions-core-jvm:$kotestVersion")
+
+    testImplementation("org.junit.jupiter:junit-jupiter-params:5.9.0")
+    testImplementation("org.testcontainers:postgresql:${tcVersion}")
+}
+
+micronaut {
+    version.set(micronautVersion)
+    testRuntime("netty")
+    testRuntime("junit5")
+    processing {
+        incremental(true)
+    }
+}
+
+application {
+    mainClass.set("no.nav.hm.grunndata.index.Application")
+}
+
+java {
+    sourceCompatibility = JavaVersion.toVersion(jvmTarget)
+    targetCompatibility = JavaVersion.toVersion(jvmTarget)
+    withSourcesJar()
+}
+
+tasks.withType<KotlinCompile> {
+    kotlinOptions.jvmTarget = jvmTarget
+}
+
+tasks.named<KotlinCompile>("compileTestKotlin") {
+    kotlinOptions.jvmTarget = jvmTarget
+}
+
+tasks.withType<Test> {
+    useJUnitPlatform()
+    testLogging {
+        events("skipped", "failed")
+        showExceptions = true
+        showStackTraces = true
+        showCauses = true
+        exceptionFormat = TestExceptionFormat.FULL
+        showStandardStreams = true
+    }
+}
+
+tasks.withType<Wrapper> {
+    gradleVersion = "7.5.1"
+}
+
+repositories {
+    mavenLocal()
+    mavenCentral()
+    maven("https://packages.confluent.io/maven/")
+    maven("https://github-package-registry-mirror.gc.nav.no/cached/maven-release")
+
+}
+
---
+++ settings.gradle.kts
@@ -0,0 +1,1 @@
+rootProject.name = "hm-grunndata-index"
