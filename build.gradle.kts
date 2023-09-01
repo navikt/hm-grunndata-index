a9ccbddbda370d52dc5b734a5a34cb9fe39fe114
Dinh Anh Tuan Tran <dinh.anh.tuan.tran@nav.no>
2023-09-01 10:14:17 +0200

    upgrade to micronaut 4.0.5

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,7 +3,7 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 
 val jvmTarget = "17"
-val micronautVersion="4.0.3"
+val micronautVersion="4.0.5"
 val kafkaVersion = "3.2.1"
 val junitJupiterVersion = "5.9.0"
 val jacksonVersion = "2.13.4"
