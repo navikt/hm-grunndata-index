b3f8158f14fdab8c819d75a6a1b7e55f8139e604
Dinh Anh Tuan Tran <dinh.anh.tuan.tran@nav.no>
2024-01-10 10:11:37 +0100

    upgrade RR and micronaut

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,7 +3,7 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 
 val jvmTarget = "17"
-val micronautVersion="4.2.2"
+val micronautVersion="4.2.3"
 val junitJupiterVersion = "5.9.0"
 val logbackClassicVersion = "1.4.12"
 val logbackEncoderVersion = "7.3"
@@ -11,7 +11,7 @@ val tcVersion= "1.17.6"
 val mockkVersion = "1.13.4"
 val kotestVersion = "5.5.5"
 val openSearchRestClientVersion = "2.4.1"
-val rapidsRiversVersion = "202311021234"
+val rapidsRiversVersion = "202401100958"
 val grunndataDtoVersion = "202312141645"
 
 group = "no.nav.hm"
