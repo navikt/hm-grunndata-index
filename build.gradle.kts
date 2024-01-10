eeb04f968f0db99d46a372ab00ece043bc6b5b7e
Dinh Anh Tuan Tran <dinh.anh.tuan.tran@nav.no>
2024-01-10 15:34:56 +0100

    upgrade rr lib

--- build.gradle.kts
+++ build.gradle.kts
@@ -11,7 +11,7 @@ val tcVersion= "1.17.6"
 val mockkVersion = "1.13.4"
 val kotestVersion = "5.5.5"
 val openSearchRestClientVersion = "2.4.1"
-val rapidsRiversVersion = "202401100958"
+val rapidsRiversVersion = "202401101532"
 val grunndataDtoVersion = "202312141645"
 
 group = "no.nav.hm"
