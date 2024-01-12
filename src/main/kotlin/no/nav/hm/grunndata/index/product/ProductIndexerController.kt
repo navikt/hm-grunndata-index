cf89473095e7e9f7d04c60ca2fa62150edf97f5b
Kristoffer A. Iversen <kristoffer.andreas.iversen@nav.no>
2024-01-12 12:44:08 +0100

    Dont remove blocking task executer annotation

--- src/main/kotlin/no/nav/hm/grunndata/index/product/ProductIndexerController.kt
+++ src/main/kotlin/no/nav/hm/grunndata/index/product/ProductIndexerController.kt
@@ -1,10 +1,11 @@
 package no.nav.hm.grunndata.index.product
 
 import io.micronaut.http.annotation.*
+import io.micronaut.scheduling.annotation.ExecuteOn
 import org.slf4j.LoggerFactory
 
 @Controller("/internal/index/products")
-// @ExecuteOn(TaskExecutors.BLOCKING)
+@ExecuteOn(TaskExecutors.BLOCKING)
 class ProductIndexerController(private val productIndexer: ProductIndexer) {
     companion object {
         private val LOG = LoggerFactory.getLogger(ProductIndexerController::class.java)
