5afd1bf886320974308598f2223033dd9376cc44
Dinh Anh Tuan Tran <dinh.anh.tuan.tran@nav.no>
2024-01-12 12:22:21 +0100

    ExceuteOn Blocking threads on all opensearch calls

--- src/main/kotlin/no/nav/hm/grunndata/index/ReIndexAllController.kt
+++ src/main/kotlin/no/nav/hm/grunndata/index/ReIndexAllController.kt
@@ -4,11 +4,14 @@ import io.micronaut.http.annotation.Controller
 
 import io.micronaut.http.annotation.Post
 import io.micronaut.http.annotation.QueryValue
+import io.micronaut.scheduling.TaskExecutors
+import io.micronaut.scheduling.annotation.ExecuteOn
 import no.nav.hm.grunndata.index.agreement.AgreementIndexer
 import no.nav.hm.grunndata.index.product.ProductIndexer
 import no.nav.hm.grunndata.index.supplier.SupplierIndexer
 
 @Controller("/internal/index/all")
+@ExecuteOn(TaskExecutors.BLOCKING)
 class ReIndexAllController(private val productIndexer: ProductIndexer,
                            private val supplierIndexer: SupplierIndexer,
                            private val agreementIndexer: AgreementIndexer) {
