e34b3b5b8d571f5ec0b8df95f82385bbc80b25db
Kristoffer A. Iversen <kristoffer.andreas.iversen@nav.no>
2024-01-12 10:49:54 +0100

    Fixes BlockingHttpClient exceptions when doing blocking http on netty event-loop

--- src/main/kotlin/no/nav/hm/grunndata/index/ReIndexAllController.kt
+++ src/main/kotlin/no/nav/hm/grunndata/index/ReIndexAllController.kt
@@ -4,6 +4,9 @@ import io.micronaut.http.annotation.Controller
 
 import io.micronaut.http.annotation.Post
 import io.micronaut.http.annotation.QueryValue
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.runBlocking
+import kotlinx.coroutines.withContext
 import no.nav.hm.grunndata.index.agreement.AgreementIndexer
 import no.nav.hm.grunndata.index.product.ProductIndexer
 import no.nav.hm.grunndata.index.supplier.SupplierIndexer
@@ -15,9 +18,18 @@ class ReIndexAllController(private val productIndexer: ProductIndexer,
 
     @Post("/")
     fun indexProducts(@QueryValue(value = "alias", defaultValue = "false") alias: Boolean) {
-        supplierIndexer.reIndex(alias)
-        agreementIndexer.reIndex(alias)
-        productIndexer.reIndex(alias)
+        runBlocking {
+            // Why Dispatchers.IO: You are trying to run a BlockingHttpClient operation on a netty event loop thread.
+            // This is a common cause for bugs: Event loops should never be blocked. You can either mark your
+            // controller as @ExecuteOn(TaskExecutors.BLOCKING), or use the reactive HTTP client to resolve
+            // this bug. There is also a configuration option to disable this check if you are certain a
+            // blocking operation is fine here.
+            withContext(Dispatchers.IO) {
+                supplierIndexer.reIndex(alias)
+                agreementIndexer.reIndex(alias)
+                productIndexer.reIndex(alias)
+            }
+        }
     }
 
 }
--- src/main/kotlin/no/nav/hm/grunndata/index/product/IsoCategoryService.kt
+++ src/main/kotlin/no/nav/hm/grunndata/index/product/IsoCategoryService.kt
@@ -1,6 +1,9 @@
 package no.nav.hm.grunndata.index.product
 
 import jakarta.inject.Singleton
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.runBlocking
+import kotlinx.coroutines.withContext
 import no.nav.hm.grunndata.rapid.dto.IsoCategoryDTO
 import org.slf4j.LoggerFactory
 
@@ -8,7 +11,16 @@ import org.slf4j.LoggerFactory
 class IsoCategoryService(gdbApiClient: GdbApiClient) {
 
     private val isoCategories: Map<String, IsoCategoryDTO> =
-        gdbApiClient.retrieveIsoCategories().associateBy { it.isoCode }
+        runBlocking {
+            // Why Dispatchers.IO: You are trying to run a BlockingHttpClient operation on a netty event loop thread.
+            // This is a common cause for bugs: Event loops should never be blocked. You can either mark your
+            // controller as @ExecuteOn(TaskExecutors.BLOCKING), or use the reactive HTTP client to resolve
+            // this bug. There is also a configuration option to disable this check if you are certain a
+            // blocking operation is fine here.
+            withContext(Dispatchers.IO) {
+                gdbApiClient.retrieveIsoCategories().associateBy { it.isoCode }
+            }
+        }
 
     companion object {
         private val LOG = LoggerFactory.getLogger(IsoCategoryService::class.java)
