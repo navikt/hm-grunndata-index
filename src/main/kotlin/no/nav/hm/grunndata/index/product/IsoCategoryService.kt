c6fabcbf92c59dc11f12759390fef567d529765a
Kristoffer A. Iversen <kristoffer.andreas.iversen@nav.no>
2024-01-12 12:43:10 +0100

    Not necessary anymore

--- src/main/kotlin/no/nav/hm/grunndata/index/product/IsoCategoryService.kt
+++ src/main/kotlin/no/nav/hm/grunndata/index/product/IsoCategoryService.kt
@@ -11,16 +11,7 @@ import org.slf4j.LoggerFactory
 class IsoCategoryService(gdbApiClient: GdbApiClient) {
 
     private val isoCategories: Map<String, IsoCategoryDTO> =
-        runBlocking {
-            // Dispatchers.IO fixes: You are trying to run a BlockingHttpClient operation on a netty event loop thread.
-            // This is a common cause for bugs: Event loops should never be blocked. You can either mark your
-            // controller as @ExecuteOn(TaskExecutors.BLOCKING), or use the reactive HTTP client to resolve
-            // this bug. There is also a configuration option to disable this check if you are certain a
-            // blocking operation is fine here.
-            withContext(Dispatchers.IO) {
-                gdbApiClient.retrieveIsoCategories().associateBy { it.isoCode }
-            }
-        }
+        gdbApiClient.retrieveIsoCategories().associateBy { it.isoCode }
 
     companion object {
         private val LOG = LoggerFactory.getLogger(IsoCategoryService::class.java)
