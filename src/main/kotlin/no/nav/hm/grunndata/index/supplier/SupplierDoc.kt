e2aa75f87d41b1603c2558812f13b1378dea8791
Dinh Anh Tuan Tran <dinh.anh.tuan.tran@nav.no>
2024-03-07 08:29:12 +0100

    status field in suppliers

--- src/main/kotlin/no/nav/hm/grunndata/index/supplier/SupplierDoc.kt
+++ src/main/kotlin/no/nav/hm/grunndata/index/supplier/SupplierDoc.kt
@@ -2,11 +2,13 @@ package no.nav.hm.grunndata.index.supplier
 
 import no.nav.hm.grunndata.rapid.dto.SupplierDTO
 import no.nav.hm.grunndata.index.SearchDoc
+import no.nav.hm.grunndata.rapid.dto.SupplierStatus
 import java.time.LocalDateTime
 
 data class SupplierDoc(
     override val id: String,
     val identifier: String,
+    val status: SupplierStatus,
     val name: String,
     val address: String?,
     val postNr: String?,
@@ -21,7 +23,7 @@ data class SupplierDoc(
     val updated: LocalDateTime) : SearchDoc
 
 fun SupplierDTO.toDoc(): SupplierDoc = SupplierDoc(
-    id = id.toString(), identifier = identifier, name = name, address = info.address, postNr = info.postNr,
+    id = id.toString(), identifier = identifier, status = status, name = name, address = info.address, postNr = info.postNr,
     postLocation = info.postLocation, countryCode = info.countryCode, email = info.email,
     phone = info.phone, homepage = info.homepage, createdBy = createdBy, updatedBy = updatedBy,
     created = created, updated = updated
