f56d9c3d56c80415e0f1a84cf55ec82c4eff5bb5
Dinh Anh Tuan Tran <dinh.anh.tuan.tran@nav.no>
2024-05-07 09:13:31 +0200

    restarte if consumer is stopped

--- src/main/kotlin/no/nav/hm/grunndata/index/AliveController.kt
+++ src/main/kotlin/no/nav/hm/grunndata/index/AliveController.kt
@@ -2,12 +2,19 @@ package no.nav.hm.grunndata.index
 
 import io.micronaut.http.annotation.Controller
 import io.micronaut.http.annotation.Get
+import no.nav.helse.rapids_rivers.KafkaRapid
 
 @Controller("/internal")
-class AliveController {
+class AliveController(private val kafkaRapid: KafkaRapid) {
 
-    @Get("/isAlive")
-    fun alive() = "ALIVE"
+
+            @Get("/isAlive")
+    fun alive(): String {
+        if (kafkaRapid.isConsumerClosed()) {
+            throw Exception("Error: consumer is closed")
+        }
+        return "ALIVE"
+    }
 
     @Get("/isReady")
     fun ready() = "OK"
