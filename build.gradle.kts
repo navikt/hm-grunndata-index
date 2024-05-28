b39a6a72db39ff4c1954f0ab9fcef0b4ede8c7f0
Dinh Anh Tuan Tran <dinh.anh.tuan.tran@nav.no>
2024-05-28 07:51:17 +0200

    upgrade dto lib

--- build.gradle.kts
+++ build.gradle.kts
@@ -13,7 +13,7 @@ val mockkVersion = "1.13.4"
 val kotestVersion = "5.5.5"
 val openSearchRestClientVersion = "2.8.0"
 val rapidsRiversVersion = "202401101532"
-val grunndataDtoVersion = "202404220928"
+val grunndataDtoVersion = "202405271558"
 
 group = "no.nav.hm"
 version = properties["version"] ?: "local-build"
