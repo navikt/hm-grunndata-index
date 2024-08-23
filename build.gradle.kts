ce51ad1c92240027840992d5473f5b9b6c3d2173
Dinh Anh Tuan Tran <dinh.anh.tuan.tran@nav.no>
2024-08-23 08:43:44 +0200

    upgrade rr lib to 202408230837

--- build.gradle.kts
+++ build.gradle.kts
@@ -12,7 +12,7 @@ val tcVersion= "1.17.6"
 val mockkVersion = "1.13.4"
 val kotestVersion = "5.5.5"
 val openSearchRestClientVersion = "2.11.1"
-val rapidsRiversVersion = "202407010855"
+val rapidsRiversVersion = "202408230837"
 val grunndataDtoVersion = "202408151452"
 
 
