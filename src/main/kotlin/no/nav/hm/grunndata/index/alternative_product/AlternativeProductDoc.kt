4489fa1df5899e445906c58c2da90f96a18b7b51
Dinh Anh Tuan Tran <dinh.anh.tuan.tran@nav.no>
2024-09-16 09:48:30 +0200

    add support for alternative products

---
+++ src/main/kotlin/no/nav/hm/grunndata/index/alternative_product/AlternativeProductDoc.kt
@@ -0,0 +1,75 @@
+package no.nav.hm.grunndata.index.alternative_product
+
+import no.nav.hm.grunndata.rapid.dto.*
+import no.nav.hm.grunndata.index.SearchDoc
+import no.nav.hm.grunndata.index.product.IsoCategoryService
+import java.time.LocalDateTime
+import no.nav.hm.grunndata.index.product.AgreementInfoDoc
+import no.nav.hm.grunndata.index.product.AttributesDoc
+import no.nav.hm.grunndata.index.product.MediaDoc
+import no.nav.hm.grunndata.index.product.ProductSupplier
+import no.nav.hm.grunndata.index.product.toDoc
+
+data class AlternativeProductDoc(
+    override val id: String,
+    val supplier: ProductSupplier,
+    val title: String,
+    val articleName: String,
+    val attributes: AttributesDoc,
+    val status: ProductStatus,
+    val hmsArtNr: String? = null,
+    val identifier: String,
+    val supplierRef: String,
+    val isoCategory: String,
+    val isoCategoryTitle: String? = null,
+    val isoCategoryTitleShort: String? = null,
+    val isoCategoryText: String? = null,
+    val isoCategoryTextShort: String? = null,
+    val seriesId: String? = null,
+    val data: List<TechData> = emptyList(),
+    val media: List<MediaDoc> = emptyList(),
+    val created: LocalDateTime,
+    val updated: LocalDateTime,
+    val expired: LocalDateTime,
+    val agreements: List<AgreementInfoDoc> = emptyList(),
+    val hasAgreement: Boolean = false,
+) : SearchDoc
+
+
+fun ProductRapidDTO.toDoc(isoCategoryService: IsoCategoryService): AlternativeProductDoc = try {
+    val onlyActiveAgreements =
+        agreements.filter { it.published!!.isBefore(LocalDateTime.now())
+                && it.expired.isAfter(LocalDateTime.now()) && it.status == ProductAgreementStatus.ACTIVE}
+    val iso = isoCategoryService.lookUpCode(isoCategory)
+    AlternativeProductDoc(id = id.toString(),
+        supplier = ProductSupplier(
+            id = supplier.id.toString(), identifier = supplier.identifier, name = supplier.name
+        ),
+        title = title,
+        articleName = articleName,
+        attributes = attributes.toDoc(),
+        status = status,
+        hmsArtNr = hmsArtNr,
+        identifier = identifier,
+        supplierRef = supplierRef,
+        isoCategory = isoCategory,
+        isoCategoryTitle = iso?.isoTitle,
+        isoCategoryTitleShort = iso?.isoTitleShort,
+        isoCategoryText = iso?.isoText,
+        isoCategoryTextShort = iso?.isoTextShort,
+        seriesId = seriesIdentifier ?: seriesUUID.toString(),
+        data = techData,
+        media = media.map { it.toDoc() }.sortedBy { it.priority },
+        created = created,
+        updated = updated,
+        expired = expired,
+        agreements = onlyActiveAgreements.map { it.toDoc() },
+        hasAgreement = onlyActiveAgreements.isNotEmpty(),
+    )
+} catch (e: Exception) {
+    println(isoCategory)
+    throw e
+}
+
+
+private fun String.decimalToInt(): Int? = substringBeforeLast(".").toInt()
---
+++ src/main/kotlin/no/nav/hm/grunndata/index/alternative_product/AlternativeProductIndexerController.kt
@@ -0,0 +1,27 @@
+package no.nav.hm.grunndata.index.alternative_product
+
+import io.micronaut.http.annotation.*
+import io.micronaut.scheduling.TaskExecutors
+import io.micronaut.scheduling.annotation.ExecuteOn
+import org.slf4j.LoggerFactory
+
+@Controller("/internal/index/alternative-products")
+@ExecuteOn(TaskExecutors.BLOCKING)
+class AlternativeProductIndexerController(private val alternativeProductIndexer: AlternativeProductIndexer) {
+    companion object {
+        private val LOG = LoggerFactory.getLogger(AlternativeProductIndexerController::class.java)
+    }
+
+    @Post("/")
+    fun indexProducts(@QueryValue(value = "alias", defaultValue = "false") alias: Boolean) {
+        alternativeProductIndexer.reIndex(alias)
+    }
+
+    @Put("/alias/{indexName}")
+    fun aliasProducts(indexName: String) {
+        alternativeProductIndexer.updateAlias(indexName)
+    }
+
+    @Get("/alias")
+    fun getAlias() = alternativeProductIndexer.getAlias()
+}
