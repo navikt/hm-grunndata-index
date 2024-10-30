af5caea345220014eb58d4ed85e4ed7d83e4d3c4
Dinh Anh Tuan Tran <dinh.anh.tuan.tran@nav.no>
2024-10-30 16:24:31 +0100

    upgrade rr and kafka

--- build.gradle.kts
+++ build.gradle.kts
@@ -12,7 +12,7 @@ val tcVersion= "1.17.6"
 val mockkVersion = "1.13.4"
 val kotestVersion = "5.5.5"
 val openSearchRestClientVersion = "2.11.1"
-val rapidsRiversVersion = "202410171601"
+val rapidsRiversVersion = "202410290928"
 val grunndataDtoVersion = "202409181446"
 val leaderElectionVersion = "202405151234"
 
